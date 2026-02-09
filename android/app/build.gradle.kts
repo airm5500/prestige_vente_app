9161dd5309738306646247c6886b6f7add306df6
airman <hermannzi@outlook.fr>
2026-02-09 15:31:07 +0000

    retour champ modif prix vente carnet

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -33,7 +33,7 @@ android {
         // MODIFICATION : Ces lignes DOIVENT être décommentées.
         // Flutter les remplit automatiquement pendant la compilation.
         versionCode = flutter.versionCode
-        versionName = "3.0.8"
+        versionName = "3.0.9"
     }
 
     buildTypes {
